f6d7a527ecc0d999c4caf2f1fad00a3233dcc982
erik savage <erik.k.savage@gmail.com>
2021-12-04 10:04:02 -0800

    updates componentDidMount() and loadResumeFromPath() to utlize primary lng json

--- src/App.js
+++ src/App.js
@@ -6,52 +6,27 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Home from "./components/Home";
+// import resumePath from "./public/res_primaryLanguage.json";
 
 class App extends Component {
 
   constructor(props) {
     super();
     this.state = {
-      foo: "bar",
+      // foo: "bar",
       resumeData: {},
       sharedData: {},
     };
   }
 
-  applyPickedLanguage = (pickedLanguage, oppositeLangIconId) => {
-    this.swapCurrentlyActiveLanguage(oppositeLangIconId);
-    document.documentElement.lang = pickedLanguage;
-    var resumePath =
-      document.documentElement.lang === window.$primaryLanguage
-        ? `res_primaryLanguage.json`
-        : `res_secondaryLanguage.json`;
-    this.loadResumeFromPath(resumePath);
-  }
-
-  swapCurrentlyActiveLanguage = (oppositeLangIconId) => {
-    var pickedLangIconId =
-      oppositeLangIconId === window.$primaryLanguageIconId
-        ? window.$secondaryLanguageIconId
-        : window.$primaryLanguageIconId;
-    document
-      .getElementById(oppositeLangIconId)
-      .removeAttribute("filter", "brightness(40%)");
-    document
-      .getElementById(pickedLangIconId)
-      .setAttribute("filter", "brightness(40%)");
-  }
-
   componentDidMount = () => {
     this.loadSharedData();
-    this.applyPickedLanguage(
-      window.$primaryLanguage,
-      window.$secondaryLanguageIconId
-    );
+    this.loadResumeFromPath();
   }
 
-  loadResumeFromPath = (path) => {
+  loadResumeFromPath = () => {
     $.ajax({
-      url: path,
+      url: "res_primaryLanguage.json",
       dataType: "json",
       cache: false,
       success: function (data) {
